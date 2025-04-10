07734111ab793172204c0a50faafa0fae772f258
Arpan Kundu <162029725+ARPANkundu2404@users.noreply.github.com>
2025-04-10 14:06:14 +0530

    env file

--- rural-dev/src/utils/firebase.js
+++ rural-dev/src/utils/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyAoF7u5ZrEClVpBEftPWcgqngm2f91f3TE",
-  authDomain: "ruraldev-b78b5.firebaseapp.com",
-  projectId: "ruraldev-b78b5",
-  storageBucket: "ruraldev-b78b5.appspot.com",
-  messagingSenderId: "347370620633",
-  appId: "1:347370620633:web:6362c3c6658df816f4a0c8",
-  measurementId: "G-03FK5LB92G",
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
